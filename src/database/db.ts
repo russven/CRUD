faa124666d938fce158a588944030b7eece6de13
russven <r.estiven98@gmail.com>
2024-12-15 22:39:30 -0500

    Configurar scripts de build y start para producción

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,19 +1,11 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
-  connectionString: 'postgres://rus:WehtjaO9XOOAQCxMx8TjZTH5zJ9Grysm@dpg-ctehoq5ds78s73df13e0-a.oregon-postgres.render.com:5432/mydatab_t5fn',
-  ssl: {
-    rejectUnauthorized: false, // Render requiere conexiones SSL
-  },
-});
-
-// Prueba de conexión
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error al conectar a la base de datos:', err.stack);
-  }
-  console.log('Conexión exitosa a PostgreSQL');
-  release();
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Necesario para conexiones a Render
 });
 
 export default pool;
